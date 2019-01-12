5eb88b158c9f0e1e42735566a5cf656ef8901870
lavrandge <lavrandge@gmail.com>
2019-01-12 19:52:49 +0300

    Dispatcher created

---
+++ src/main/java/com/telran/m2m/dto/MessageData.java
@@ -0,0 +1,40 @@
+package com.telran.m2m.dto;
+
+public class MessageData {
+    /**
+     * Timestamp of room data object
+     */
+    public long timestamp;
+
+    /**
+     * Room object
+     */
+    public String text;
+
+    /**
+     * Critical message flag
+     */
+    public boolean critical;
+
+    public MessageData(long timestamp, String text, boolean critical) {
+        this.timestamp = timestamp;
+        this.text = text;
+        this.critical = critical;
+    }
+
+    public MessageData() {
+    }
+
+    @Override
+    public String toString() {
+        return "MessageData{" +
+                "timestamp=" + timestamp +
+                ", text='" + text + '\'' +
+                ", critical=" + critical +
+                '}';
+    }
+
+    public boolean isCritical() {
+        return critical;
+    }
+}
